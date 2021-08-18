fb72e5fdc83758b89f350944acec84e0667fe9ac
vlearnsir <goftogocenter@gmail.com>
2021-08-18 12:35:22 +0430

    Add project files.

---
+++ PersianNumberCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA_NumberToString
+{
+    public class PersianNumberCollection
+    {
+        private static readonly string[] _Level_A = {
+            "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه"
+        };
+        private static readonly string[] _Level_B = {
+            "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده"
+        };
+        private static readonly string[] _Level_C = {
+            "ده", "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود"
+        };
+        private static readonly string[] _Level_D = {
+            "یکصد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد"
+        };
+        private static readonly string[] _Level_E = {
+            "هزار", "میلیون", "میلیارد", "بیلیون", "بیلیارد", "تریلیون", "تریلیارد"
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FA_NumberToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
